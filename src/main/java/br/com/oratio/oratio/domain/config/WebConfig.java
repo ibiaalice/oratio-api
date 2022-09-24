d4c707311600c0b50c983c2cc513be94caf674d2
Bia Alice <bia.alice@softdesign.com.br>
2022-09-24 18:53:06 -0300

    feat: add web config

---
+++ src/main/java/br/com/oratio/oratio/domain/config/WebConfig.java
@@ -0,0 +1,16 @@
+package br.com.oratio.oratio.domain.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
+
+    
+}
